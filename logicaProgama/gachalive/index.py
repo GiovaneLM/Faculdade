0e3b30071e0a2c64665f1073e267aec126d2ac9d
GiovaneLM <giovanelimeiramartins@gmail.com>
2026-06-01 11:36:52 -0300

    gacha life

---
+++ logicaProgama/gachalive/index.py
@@ -0,0 +1,18 @@
+missoes=[]
+recompensa=[]
+nome=""
+pontos=0
+nome=input("ola usuario nos informe seu nome: ")
+
+while True:
+    print(f'Nome: {nome} \nPontos: {pontos}')
+    print('1-listar missoes')
+
+    menu=int(input('O que deseja fazer: '))
+    match menu:
+        case 1:
+            for i in range(len(missoes)):
+                print(f"\nmissao: {missoes[i]}\n")
+        case 2:
+            missao=input("Digite a missao que deseja: ")
+            missoes.append(missao)
