7a476d3f16e8a3a3eff5730401f4fbfa2503287f
GiovaneLM <giovanelimeiramartins@gmail.com>
2026-06-19 11:56:04 -0300

    .......

--- semestre1/logicaProgama/aula013/index2.py
+++ semestre1/logicaProgama/aula013/index2.py
@@ -1,7 +1,7 @@
 lista=[]
 while True:
     for i in range(8):
-        numero=int(input(f"{i} numero: "))
+        numero=int(input("numero: "))
         if len(lista) >= 8:
             lista.pop(0)
         lista.append(numero)           
